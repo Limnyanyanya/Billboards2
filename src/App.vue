f826c2eeb94d74850dc7be3d814800a65769e04e
Limnyanyanya <3303007240@qq.com>
2023-10-14 20:25:37 +0800

    初次上传，仅为测试

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <html>
+    <head>
+<meta charset="utf-8">
+<title>Billboards</title>
+    </head>
+    <body>
+<h1>告示牌</h1>
+<p>这是一个告示牌</p>
+    </body>
+    </html>
+</template>
+<script setup lang="ts">
+</script>
+<style></style>
